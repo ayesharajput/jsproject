4152cca3548acd74ae6f174621a2e71f461ebeed
ayesha <aishalay66777@gmail.com>
2023-09-06 14:04:33 +0500

    commit

---
+++ app.js
@@ -0,0 +1,100 @@
+// Chapter 01((Alert)
+// QUESTION 01
+
+var firstName ="Ayesha";
+var lastName ="Ali";
+var  Email ="abc@gamil.com";
+var phoneNo ="0306757464636";
+var Password ="12575098";
+alert(firstName);
+alert(lastName);
+alert(phoneNo);
+alert(Email);
+alert(Password);
+ 
+// Question 02
+alert(" alert You're learning JavaScript!")
+
+// QUESTION 03
+alert(" welcome")
+
+
+
+// Chapter 2 (Variables for string)
+// question 01,02
+var productName;
+productName = "iphone";
+document.write(productName);
+
+// question 03,04
+var  teamName = "Pakistan"
+document.write(teamName);
+var bestMan = "Charlie";
+document.write(bestMan);
+
+// Chapter Chapter 3 (Variables for numbers)
+
+// QUESTION 01
+var caseQty =133;
+caseQty = 144;
+//question no 04
+var sum;
+sum = 2 + 7;
+
+
+//question no 04
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+document.write(orderTotal);
+// question no 06
+var num =3;
+var num2 = "5";
+document.write(num+num2);
+
+
+//Chapter 4 (Variable names Legal and Illegal)
+//Q1
+var productcost = 3.45;
+//Q2
+var nameOfBand;
+// Q3
+var legally_num=5;
+//Q4
+var firstName = "Ayesha";
+var lastName = "Ali";
+var Fullname = firstName + " " + lastName;
+document.write(Fullname);
+//Q5
+// A letter, dollar sign($), or underscore (_) must make up the first character. A number cannot be the initial character. Any letter, number, or underscore can complete the variable name. You cannot use some characters, such as spaces, symbols, and punctuation
+
+//Chapter 5 (Math Expression I)
+
+// // Q1
+// 1. What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another? 
+// %
+// Q2
+
+console.log(20 % 6);
+
+// Q3
+var largeNum = 1000*2000;
+console.log(largeNum)
+//Q4
+var number = 1000;
+number=number-300;
+console.log(number);
+// Q5
+var dividend = 12;
+var divisor = 7;
+var remainder = dividend % divisor;
+
+alert(dividend + " divided by " + divisor );
+
+
+//question 6
+var num1 = 5;
+var num2 = 8;
+var multiplicationResult = num1 * num2;
+alert("result  " + num1 + " and " + num2 );
